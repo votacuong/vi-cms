5c550f44911a71c052d1222fa1a7d4f6f53ea174
Võ Tá Cương <162108993+votacuong@users.noreply.github.com>
2025-04-24 10:23:01 +0700

    Update Common_helper.php

--- app/Helpers/Common_helper.php
+++ app/Helpers/Common_helper.php
@@ -118,38 +118,8 @@ function uploadFile($id, $name, $sub)
 {
 	$target_dir = dirname(dirname(dirname(__FILE__)))."/public/images/".$sub.'/';
 	$target_file = $target_dir . $id.'.png';
-	$uploadOk = 1;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"]) && is_file($_FILES[$name]["tmp_name"]) ) {
-	  $check = getimagesize($_FILES[$name]["tmp_name"]);
-	  if($check !== false) {
-		$uploadOk = 1;
-	  } else {
-		$uploadOk = 0;
-	  }
-	}
-
-
-	// Check file size
-	if ($_FILES[$name]["size"] > 500000) {
-	  $uploadOk = 0;
-	}
-
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-	  $uploadOk = 0;
-	}
-
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-	// if everything is ok, try to upload file
+	if (move_uploaded_file($_FILES[$name]["tmp_name"], $target_file)) {
 	} else {
-	  if (move_uploaded_file($_FILES[$name]["tmp_name"], $target_file)) {
-	  } else {
-	  }
 	}
 }
 function generateRandomString($length = 10) {
