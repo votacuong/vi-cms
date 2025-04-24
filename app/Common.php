43b2ae6eb185b3a3d3d67586039ec1471c91c69e
Võ Tá Cương <162108993+votacuong@users.noreply.github.com>
2025-04-24 11:21:06 +0700

    First path

--- app/Common.php
+++ app/Common.php
@@ -55,26 +55,31 @@ class VLang{
 	public static function __($code)
 	{
 		
-		if (!isAdmin()){
-			$db = \Config\Database::connect();
+		if ( count( self::$langs ) == 0 )
+		{
 			
-			$builder = $db->table( "app_users" );
+			if (!isAdmin()){
+				$db = \Config\Database::connect();
+				
+				$builder = $db->table( "app_users" );
+				
+				$builder->where( 'id', session()->get('id') );
 			
-			$builder->where( 'id', session()->get('id') );
-		
-			$data = (array)$builder->get()->getRow();
-			
-			if (empty($data['language'])){
-				$language = 'en-GB';
-			}else{
-				$language = $data['language'];
+				$data = (array)$builder->get()->getRow();
+				
+				if (empty($data['language'])){
+					$language = 'en-GB';
+				}else{
+					$language = $data['language'];
+				}
+				
+				self::$langs = parse_ini_file( dirname(__FILE__). '/Language/countries/'.$language.'.ini' );
+			}else{				
+				$AppConfig = new \Config\AppConfig();
+				self::$langs = parse_ini_file( dirname(__FILE__). '/Language/countries/'.$AppConfig->system_language.'.ini' );
+
 			}
 			
-			self::$langs = parse_ini_file( dirname(__FILE__). '/Language/countries/'.$language.'.ini' );
-		}else{				
-			$AppConfig = new \Config\AppConfig();
-			self::$langs = parse_ini_file( dirname(__FILE__). '/Language/countries/'.$AppConfig->system_language.'.ini' );
-
 		}
 		
 		return isset( self::$langs[$code] ) ? self::$langs[$code] : $code;
