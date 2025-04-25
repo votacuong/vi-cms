0a39afe6b9eca1c006ccedda2cbd6d1f2c93f394
Võ Tá Cương <162108993+votacuong@users.noreply.github.com>
2025-04-25 16:02:39 +0700

    d

--- app/Config/AppConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Config;
-
-class AppConfig{
-	public $system_language = 'en-GB';
-	public $mailfrom = 'votacuong2608@gmail.com';
-	public $google_app_password = 'rlsp kxyu txjm lfqr';
-}
